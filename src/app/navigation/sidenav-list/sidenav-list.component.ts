95ba2d942221926e296fbdfd9c52bfb80f5bf15c
Thibaut MANDALLENA <tmandallena@squad.fr>
2021-12-10 20:53:31 +0100

    Organisation of app.component

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.scss'],
+})
+export class SidenavListComponent implements OnInit {
+  @Output() linkClick = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onLinkClick(): void {
+    this.linkClick.emit();
+  }
+}
